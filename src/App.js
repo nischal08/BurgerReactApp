ec2a859576a6ed06ee0149392047a4920ed0b021
nischal08 <nischal.karanjit@gmail.com>
2020-05-09 14:57:02 +0545

    fetch order from database and displayed it

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/BurgerBuilder/Checkout/Checkout';
 import Orders from './containers/Orders/Orders'
-import order from './components/Order/Order';
+
 class App extends Component {
   render() {
     return (
